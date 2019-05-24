951ec12a898ca3d09b0d348c3191d9f4c44681fc
Ricardo Bartels <ricardo@bitchbrothers.com>
2019-05-24 19:23:06 +0200

    added logging and command line arguments

--- icinga-bot.py
+++ icinga-bot.py
@@ -3,22 +3,45 @@
 # This is mostly taken from:
 # https://github.com/slackapi/python-slackclient/blob/master/tutorial/02-building-a-message.md
 
+#################
+#
+#   import standard modules
+#
+
 import os
 import re
 import logging
 import asyncio
 import ssl as ssl_lib
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
 
-import certifi
-import slack
-
-from icinga2api.client import Client as i2_client
 
 # use while debugging
 import pprint
 
+
+#################
+#
+#   import extra modules
+#
+
+import certifi
+import slack
+from icinga2api.client import Client as i2_client
+
+
 __version__ = "0.0.1"
+__version_date__ = "2019-05-24"
 __author__ = "Ricardo Bartels <ricardo@bitchbrothers.com>"
+
+
+#################
+#
+#   default vars
+#
+
+default_log_level = "INFO"
+default_config_file_path = "./icinga-bot.ini"
 
 SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
 
@@ -27,8 +50,90 @@ ICINGA2_PORT = os.environ["ICINGA2_PORT"]
 ICINGA2_USER = os.environ["ICINGA2_USER"]
 ICINGA2_PASS = os.environ["ICINGA2_PASS"]
 
+#################
+#
+#   INTERNAL VARS
+#
+
+# define a program description
+self_description = "ICINGA BOT DESCRIPTION"
+
 MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
 
+# define valid log levels
+valid_log_levels = [ "DEBUG", "INFO", "WARNING", "ERROR"]
+
+args = None
+log = None
+
+
+#################
+#
+#   FUNCTIONS
+#
+
+# parse command line arguments
+def parse_command_line():
+
+    # define command line options
+    parser = ArgumentParser(description=self_description + "\nVersion: " + __version__ + " (" + __version_date__ + ")", formatter_class=RawDescriptionHelpFormatter)
+
+    parser.add_argument("-c", "--config", default=default_config_file_path, dest="config_file",
+                        help="points to the config file to read config data from which is not installed under the default path '" + default_config_file_path + "'" ,
+                        metavar="icinga-bot.ini")
+    parser.add_argument("-l", "--log_level", choices=valid_log_levels, dest="log_level",
+                        help="set log level (overrides config)")
+    parser.add_argument("-d", "--daemon",  action='store_true', dest="daemon",
+                        help="define if the script is run as a systemd daemon")
+
+    return parser.parse_args()
+
+# log an error and exit with error level 1
+def do_error_exit(log_text):
+    global log
+    if log:
+        log.error(log_text)
+    else:
+        logging.error(log_text)
+    exit(1)
+
+# setup logging
+def setup_logging(log_level):
+
+    global args
+
+    logger = None
+
+    # define log format first
+    if args.daemon:
+        # ommit time stamp if run in daemon mode
+        logging.basicConfig(level="DEBUG", format='%(levelname)s: %(message)s')
+    else:
+        logging.basicConfig(level="DEBUG", format='%(asctime)s - %(levelname)s: %(message)s')
+
+    if log_level == None or log_level == "":
+        logging.debug("Configuring logging: No log level defined, using default level: %s" % default_log_level)
+        log_level = default_log_level
+    else:
+        logging.debug("Configuring logging: Setting log level to: %s" % log_level)
+
+
+    # create logger handler
+    logger = logging.getLogger(__name__)
+
+    # check set log level against self defined log level array
+    if not log_level.upper() in valid_log_levels:
+        do_error_exit('Invalid log level: %s' % log_level)
+
+    # check the provided log level and bail out if something is wrong
+    numeric_log_level = getattr(logging, log_level.upper(), None)
+    if not isinstance(numeric_log_level, int):
+        do_error_exit('Invalid log level: %s' % log_level)
+
+    # set handler log level
+    logger.setLevel(numeric_log_level)
+
+    return logger
 
 def enum(*sequential, **named):
     enums = dict(zip(sequential, range(len(sequential))), **named)
@@ -119,9 +224,16 @@ async def message(**payload):
     return
 
 if __name__ == "__main__":
-    logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)
-    logger.addHandler(logging.StreamHandler())
+
+    ################
+    #   parse command line
+    args = parse_command_line()
+
+    ################
+    #   setup logging
+    log = setup_logging(args.log_level)
+
+    log.info("Starting " + self_description)
 
     # set up icinga
     icinga2_client = i2_client("https://" + ICINGA2_HOST + ":" + ICINGA2_PORT, ICINGA2_USER, ICINGA2_PASS)
